f938342f8b6534c53cf686d9490ec042ce42b9d4
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 10:44:29 +0400

    Create InitialMood.java

---
+++ InitialMood.java
@@ -0,0 +1,16 @@
+//strategy
+public abstract class InitialMood implements State { 
+
+	//Common for Suhaib and Butler
+	public void prev(Mood m) {
+		// TODO Auto-generated method stub
+		System.out.println("How can I go to my previous mood if this is my initial??");
+	}
+
+	//Unique to Suhaib and Butler
+	public abstract void next(Mood m);
+
+	//Unique to Suhaib and Butler
+	public abstract void printStatus();
+	
+}
