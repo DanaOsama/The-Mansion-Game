b4225edfb5a9992ccdd983b6cc247cfed7470972
Myth <64269445+maaytha@users.noreply.github.com>
2020-05-08 19:42:10 +0400

    Update Butler.java

--- Butler.java
+++ Butler.java
@@ -1,6 +1,8 @@
   
 public class Butler extends Character{
 
+	Boolean[] Keys = {false, false, false, false, false};
+	
 	public Butler(Scene currentScene, String name) {
 		super(currentScene, name);
 		m = new ButlerInitialMood();
