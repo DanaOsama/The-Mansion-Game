49b5524a9bc097adb41f948c3823f69bc499968d
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 12:19:04 +0400

    Update Suhaib.java

--- Suhaib.java
+++ Suhaib.java
@@ -1,1 +1,13 @@
+public class Suhaib extends Character{
 
+	public Suhaib(Scene currentScene, String name) {
+		super(currentScene, name);
+		m = new SuhaibInitialMood();
+	}
+	
+	public void run()
+	{
+		
+	}
+
+}
