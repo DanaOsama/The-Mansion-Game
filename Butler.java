7c71b04f2d07ecbb00728e4fc78204e0c69c97db
Myth <64269445+maaytha@users.noreply.github.com>
2020-05-08 20:07:51 +0400

    Update Butler.java

--- Butler.java
+++ Butler.java
@@ -1,16 +1,32 @@
-  
+ //singleton
 public class Butler extends Character{
 
 	Boolean[] Keys = {false, false, false, false, false};
 	
-	public Butler(Scene currentScene, String name) {
-		super(currentScene, name);
+	private Butler instance = null;
+	
+	private Butler() {
+		// SET SCENE FOR BUTLER
+		super(currentScene, "Butler");
 		m = new ButlerInitialMood();
 	}
 	
-	public void run()
-	{
+	public void run() {
 		
 	}
+	
+	public Butler getInstance() {
+		if (instance == null) {
+			instance = new Butler();
+		}
+		return instance;
+	}
+	
+	
+	public void giveKey(int keyNum) {
+		Keys.(keyNum-1) = true;
+		System.out.println("You have successfully collected key number " + keyNum );
+	}
+	
 
 }
