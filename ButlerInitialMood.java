e9f29ba4592dd9794346afa42d1197eeee3b6eb5
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 10:47:10 +0400

    Create ButlerInitialMood.java

---
+++ ButlerInitialMood.java
@@ -0,0 +1,14 @@
+class ButlerInitalMood extends InitialMood {
+
+	@Override
+	public void next(Mood m) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void printStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
