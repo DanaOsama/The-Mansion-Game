360a1cfc85047846dc9953aea74cc301129768f3
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 21:49:22 +0400

    Update SuhaibInitialMood.java

--- SuhaibInitialMood.java
+++ SuhaibInitialMood.java
@@ -2,8 +2,9 @@
 class SuhaibInitialMood extends InitialMood {
 	
 	public void next(Mood m) {
-		m.setMood(new HelpSuhaib());
-	}
+		if (m.PlayerInRoom) m.setMood(new HelpSuhaib());
+		else System.out.println("You are not in the room!");
+		}
 
 	public void printStatus() {
 		System.out.println("HELP! HELP! You need to figure out the combination on the door to free me!");
