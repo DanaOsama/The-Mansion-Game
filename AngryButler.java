0f9fd57b93b100014c3d8e2147208efbaef45b02
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 12:06:13 +0400

    Create AngryButler.java

---
+++ AngryButler.java
@@ -0,0 +1,13 @@
+public class AngryButler extends Angry{
+
+	public void prev(Mood m) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void next(Mood m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
