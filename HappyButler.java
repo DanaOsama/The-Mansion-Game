e94b72a495697e956d7a8d1e042667813f41716b
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 12:13:48 +0400

    Create HappyButler.java

---
+++ HappyButler.java
@@ -0,0 +1,12 @@
+public class HappyButler extends Happy{
+	public void prev(Mood m) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void next(Mood m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
