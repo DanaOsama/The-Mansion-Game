8fe603f540bed993ba003071a0977e194d3af6a7
maya-hiba <62069621+maya-hiba@users.noreply.github.com>
2020-05-08 10:38:52 +0400

    Create SuhaibInitialMood.java

---
+++ SuhaibInitialMood.java
@@ -0,0 +1,12 @@
+//strategy
+class SuhaibInitialMood extends InitialMood {
+	
+	public void next(Mood m) {
+		m.setMood(new Help());
+	}
+
+	public void printStatus() {
+		System.out.println("HELP! HELP! You need to figure out the combination on the door to free me!");
+	}
+	
+}
